785578572b0308d5b9b3b89179bdb8f1d7184de1
Huy Tran <huy@greensoftware.asia>
2024-02-20 11:59:25 +0700

    feature: more object helpers

--- src/pkg/object-helper.ts
+++ src/pkg/object-helper.ts
@@ -9,3 +9,34 @@ export function removeUndefinedProps(data: any): any {
     return aggregated;
   }, {});
 }
+
+export const tryParseStringIntoCorrectData = (data: string): any => {
+  if (!data?.length) {
+    return;
+  }
+
+  try {
+    // Parse boolean
+    if (data === "true") {
+      return true;
+    }
+
+    if (data === "false") {
+      return false;
+    }
+
+    // Parse number
+    const dataAsNumber = Number(data);
+
+    if (!Number.isNaN(dataAsNumber)) {
+      return dataAsNumber;
+    }
+
+    // Parse object
+    const parsedObject = JSON.parse(data);
+
+    return parsedObject;
+  } catch (error) {
+    return data;
+  }
+};

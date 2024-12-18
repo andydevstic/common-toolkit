9684e1731fb25a596cb91855389c6feef9517628
Huy Tran <huy@greensoftware.asia>
2024-12-18 07:47:53 +0700

    refactor: milestone steps

--- src/pkg/workflow/processing-milestone.ts
+++ src/pkg/workflow/processing-milestone.ts
@@ -9,17 +9,27 @@ export interface OnFailure {
 
 export class SingleProcessingMilestone {
   protected _steps: Step[];
+  protected _isFinished = false;
 
   constructor(
     protected pipelineName?: string,
     protected logger?: (msg: string) => void
   ) {}
 
+  protected throwIfEnded() {
+    if (this._isFinished) {
+      throw new Error("pipeline has already ended");
+    }
+  }
+
   public get totalSteps(): number {
+    this.throwIfEnded();
+
     return this._steps.length;
   }
 
   public addStep(name?: string, onFailure?: OnFailure): void {
+    this.throwIfEnded();
     const stepNo = this.totalSteps + 1;
 
     this._steps.push({
@@ -32,5 +42,13 @@ export class SingleProcessingMilestone {
     if (this.pipelineName && this.logger) {
       this.logger(`step ${stepNo} of pipeline ${this.pipelineName} reached!`);
     }
+  }
+
+  public result(): Step {
+    this.throwIfEnded();
+
+    this._isFinished = true;
+
+    return this._steps.pop();
   }
 }

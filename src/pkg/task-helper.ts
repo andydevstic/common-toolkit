2aca5fb76e715db18db9cdeb00001503bd0066a2
Huy Tran <huy@greensoftware.asia>
2025-10-21 22:07:36 +0700

    refactor: enhance run with timeout helper

--- src/pkg/task-helper.ts
+++ src/pkg/task-helper.ts
@@ -2,21 +2,36 @@ import { TaskFn } from "../interfaces";
 
 export async function runWithTimeout<T = any>(
   handler: TaskFn,
-  timeoutInMs: number
+  timeoutInMs: number,
+  onCompletedAfterReject?: (result: any) => any
 ): Promise<T> {
   return new Promise((resolve, reject) => {
-    const timeout = setTimeout(
-      () => reject(new Error("task timed out")),
-      timeoutInMs
-    );
+    let isCompleted = false;
+
+    const timeout = setTimeout(() => {
+      if (isCompleted) return;
+      isCompleted = true;
+
+      reject(new Error("task timed out"));
+    }, timeoutInMs);
 
     handler()
       .then((result: T) => {
+        if (isCompleted) {
+          onCompletedAfterReject?.(result);
+
+          return;
+        }
+        isCompleted = true;
+
         clearTimeout(timeout);
 
         resolve(result);
       })
       .catch((error: Error) => {
+        if (isCompleted) return;
+        isCompleted = true;
+
         clearTimeout(timeout);
 
         reject(error);
